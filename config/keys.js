478a90349328931276ea1c19edce378723998a59
robitica <robertvaida@hotmail.com>
2018-04-13 23:53:41 +0200

    credentials (google, mongodb, session key)

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '857668350445-g45uuqudqfc0mi0qvr459vauto55eidk.apps.googleusercontent.com',
+        clientSecret: '49Hn_J6Nt9rJzZoWnsjKcjnQ'
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://codemaster:Roby30091992@kweeni-p1vc9.mongodb.net/'
+    },
+    session: {
+        cookieKey: 'weareonfire!IMD'
+    }
+};
