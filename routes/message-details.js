98041005bc5af6b6a327c39848dbed1f48defd4e
Roel <roelifant@gmail.com>
2018-04-18 19:22:08 +0200

    show profile in header on messages-details

--- routes/message-details.js
+++ routes/message-details.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const authCheck = (req, res, next) => {
+  if(!req.user) {
+    // if user is not logged in
+    res.redirect('/');
+  } else {
+    // if logged in
+    next();
+  }
+};
+
 /* GET messege details page. */
-router.get('/', function(req, res, next) {
-  res.render('message-details', { title: 'Kweeni' });
+router.get('/', authCheck, function(req, res, next) {
+  res.render('message-details', {
+    title: "kweeni",
+    username: req.user.username,
+    picture: req.user.picture
+  });
 });
 
 module.exports = router;
