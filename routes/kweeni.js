57d82de991c85cd9af93fd4b277046db061bd6a1
CodeMaster <robertvaida@hotmail.com>
2018-04-19 12:11:36 +0200

    sass test

--- routes/kweeni.js
+++ routes/kweeni.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const User = require('../models/user-model');
 const Question = require('../models/question-model');
 const router = express.Router();
-
+/*
 const authCheck = (req, res, next) => {
   if(!req.user) {
     // if user is not logged in
@@ -12,19 +12,19 @@ const authCheck = (req, res, next) => {
     // if logged in
     next();
   }
-};
+};*/
 
 /* GET messages page. */
-router.get('/', authCheck, (req, res, next) => {
+router.get('/', /*authCheck,*/ (req, res, next) => {
   res.render('kweeni', { 
-    username: req.user.username,
-    picture: req.user.picture
+    //username: req.user.username,
+    //picture: req.user.picture
   });
 });
 
 // POST method route=
 router.post('/', (req, res) => {
-  var username = req.user.username;
+  /*var username = req.user.username;
   var question = new Question({
     question: req.body.question,
     author: req.user.id
@@ -32,8 +32,8 @@ router.post('/', (req, res) => {
   question.save(function (err) {
     Question.find({}).populate('author')
     if (err) console.log(err);
-  });
-  res.redirect('/kweeni/' + req.user.id)
+  });*/
+  res.redirect('/kweeni/test' /*req.user.id*/)
 });
 
 module.exports = router;
