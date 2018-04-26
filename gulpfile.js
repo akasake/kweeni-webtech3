4313d0f0aa658811ce5f61490657407ac93193ff
Roel <roelifant@gmail.com>
2018-04-26 12:26:35 +0200

    added imagemin and nodemon task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 const gulp = require("gulp");
-var sass = require('gulp-sass');
+const sass = require('gulp-sass');
+const nodemon = require('gulp-nodemon');
+const imagemin = require('gulp-imagemin');
 
-gulp.task('default', ['sass:watch']);
+gulp.task('default', ['sass:watch', "imagemin", "nodemon"]);
 
 gulp.task('sass', function () {
-    return gulp.src('./sass/style.scss')
+    return gulp.src('./src/sass/style.scss')
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
       .pipe(gulp.dest('./public/stylesheets'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
+
+gulp.task('imagemin', () =>
+    gulp.src('./src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./public/images'))
+);
+
+gulp.task('nodemon', function () {
+    nodemon({
+      script: './bin/www'
+    , ext: 'js pug'
+    , env: { 'NODE_ENV': 'development' }
+    })
+  })
