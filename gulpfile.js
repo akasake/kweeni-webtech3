ca7d95da48f6c922b4846cc8e3ca3c0ab033d427
Akasake <pham_see@hotmail.com>
2018-05-15 14:02:56 +0200

    livereload

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ const gulp = require("gulp");
 const sass = require('gulp-sass');
 const nodemon = require('gulp-nodemon');
 const imagemin = require('gulp-imagemin');
+const less = require('gulp-less');
+const livereload = require('gulp-livereload');
+let path = require('path');
 
-gulp.task('default', ['sass:watch', "imagemin", "nodemon"]);
+gulp.task('default', ['sass:watch', "imagemin", "nodemon", "less", "watch"]);
 
 gulp.task('sass', function () {
     return gulp.src('./src/sass/style.scss')
@@ -27,4 +30,17 @@ gulp.task('nodemon', function () {
     , ext: 'js pug'
     , env: { 'NODE_ENV': 'development' }
     })
-  })
+  });
+
+  gulp.task('less', function() {
+    gulp.src('less/*.less')
+      .pipe(less())
+      .pipe(gulp.dest('./public/stylesheets'))
+      .pipe(livereload());
+    });
+
+   
+  gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('less/*.less', ['less']);
+  });
