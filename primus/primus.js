146b74563fd333d6553164dc85d9074bc3790419
Akasake <pham_see@hotmail.com>
2018-05-15 19:11:16 +0200

    setup primus to recieve info from scripts.js

--- primus/primus.js
+++ primus/primus.js
@@ -1,13 +1,53 @@
+const User = require('../models/user-model');
+const Question = require('../models/question-model');
+const mongoose = require('mongoose');
+var Primus = require('primus');
+var Rooms = require('primus-rooms');
+var server = require('http').createServer();
+
 exports.kickstart = function(server) {
     const Primus = require('primus');
-    let primus = new Primus(server, {});
+    let primus = new Primus(server, { transformer: 'websockets' });
+
+    // add rooms to Primus
+    primus.plugin('rooms', Rooms);
 
     primus.on('connection', function(spark) {
-        // primus.emit -> alle connecties
-        // spark.emit -> een connectie
         console.log("Spark connected");
-        spark.on('data', function(data) {
-            primus.write(data);
-        });
+        // get room from url
+        var room = spark.query.room;
+        // check if spark is already in this room
+        if (~spark.rooms().indexOf(room)) {
+            send();
+        } else {
+            // join the room
+            spark.join(room, function(){
+            send();
+            });
+        }
+        
+        // send to the user in the room
+        function send() {
+            spark.on('data', function(data) {
+                    // basic front end info doorsturen naar frontend
+                    User.findOne({ _id: data.userId }, function (err, user) {
+                        data.username = user.username;
+                        data.userPicture = user.picture;
+                        Question.findOne({ _id: data.questionId }, function (err, question) {
+                            data.likesCount = question.likes.length+1;
+                            spark.room(room).write(data);
+                        });
+                    });
+
+                    // backend info doorsturen naar database
+                    if(data.btn) {
+                       // run dit als comment button wordt ingedrukt
+                    } else if(data.like) {
+                       // run dit als like wordt gedurkt
+                    } else {
+                       // run dit als het een subcomment is
+                    }
+            });
+        }
     });
 } // 2 primus
