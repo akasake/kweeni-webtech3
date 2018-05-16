ef282484da6a3058c37f06574f9a6951defe519c
Roel <roelifant@gmail.com>
2018-05-16 17:39:22 +0200

    added dates to questions

--- models/question-model.js
+++ models/question-model.js
@@ -6,7 +6,8 @@ const questionSchema = new Schema({
     question: String,
     slug: String,
     likes: Number,
-    author: { type: Schema.Types.ObjectId, ref: 'user' }
+    author: { type: Schema.Types.ObjectId, ref: 'user' },
+    date: Date
 });
 
 const Question = mongoose.model('question', questionSchema);
