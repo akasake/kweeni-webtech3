36edb5abea971616be3de397b2b3c470d195215b
Roel <roelifant@gmail.com>
2018-05-16 17:39:22 +0200

    added dates to questions

--- routes/kweeni.js
+++ routes/kweeni.js
@@ -24,19 +24,20 @@ router.get('/', authCheck, (req, res, next) => {
 });
 
 // GET messege details page with the user id
-router.get('/:question', function(req, res, next) {
+router.get('/:question', authCheck, function(req, res, next) {
   Question.findOne({slug: req.params.question}).populate('author').exec(function(err, question) {
     if(err) {
       console.log(err);
       res.send("404");
     } else {
-      console.log("author = "+question.author.username);
+      
       res.render('kweeni-details', {
         username: req.user.username,
         userPicture: req.user.picture,
         question: '"'+question.question+'"',
         postername: question.author.username,
-        posterPicture: question.author.picture
+        posterPicture: question.author.picture,
+        date: dateNotation(question.date)+" gevraagd door"
       });
     }
   });
@@ -54,8 +55,10 @@ router.post('/', (req, res) => {
     question: req.body.question,
     author: req.user.id,
     slug: slugQuestion,
-    likes: 0
+    likes: 0,
+    date: Date.now()
   });
+  console.log(Date.now());
   question.save(function (err) {
     Question.findOne({}).populate('author').exec(function (err, question) {
     if (err) console.log(err);
@@ -63,5 +66,45 @@ router.post('/', (req, res) => {
 });
   res.redirect('/kweeni/' + slugQuestion)
 });
+
+function dateNotation(date){
+  var milliseconds = (Date.now()-date);
+  var seconds = Math.floor(milliseconds/1000);
+  var minutes = Math.floor(seconds/60);
+  var hours = Math.floor(minutes/60);
+  var days = Math.floor(hours/60);
+  var weeks = Math.floor(days/7);
+  var months = Math.floor(days/31);
+  var years = Math.floor(days/365)
+
+  if(years>0){
+    var result = years+" jaar geleden"
+  } else if(months>1){
+    var result = months+" maanden geleden"
+  } else if(months>0){
+    var result = months+" maand geleden"
+  } else if(weeks>1){
+    var result = weeks+" weken geleden"
+  } else if(weeks>0){
+    var result = weeks+" week geleden"
+  } else if(days>1){
+    var result = days+" dagen geleden"
+  } else if(days>0){
+    var result = days +" dag geleden"
+  } else if(hours>0){
+    var result = hours+" uur geleden"
+  } else if(minutes>1){
+    var result = minutes+" minuten geleden"
+  } else if(minutes>0){
+    var result = minutes+" minuut geleden"
+  } else if(seconds>0){
+    var result = seconds+" seconden geleden"
+  } else {
+    var result = "zonet"
+  }
+
+  //return milliseconds+"ms "+seconds+"s "+minutes+"m "+hours+"h "+days+"d "+weeks+"weken "+months+"maanden "+years+"jaar";
+  return result;
+}
 
 module.exports = router;
