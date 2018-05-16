54a3077f10e2e413db9b6df6cf3938f1564ba0e8
Akasake <pham_see@hotmail.com>
2018-05-16 22:57:41 +0200

    set up local connection

--- routes/kweeni.js
+++ routes/kweeni.js
@@ -35,7 +35,7 @@ router.get('/', authCheck, (req, res, next) => {
 });
 
 // GET messege details page with the user id
-router.get('/:question', authCheck, function(req, res, next) {
+router.get('/:question', function(req, res, next) {
   Question.findOne({slug: req.params.question}).
   populate('author').
   populate('comment.postedBy').
@@ -45,23 +45,24 @@ router.get('/:question', authCheck, function(req, res, next) {
     if(err) { 
       res.send("404");
     } else {
-      res.render('kweeni-details', {
+      res.render('kweeni-details', { 
         username: req.user.username,
         userId: req.user.id,
-        userPicture: req.user.picture,
-        question: '"'+question.question+'"',
-        postername: question.author.username,
-        posterPicture: question.author.picture,
+        picture: req.user.picture,
+        question: question.question,
+        authorName: question.author.username,
+        authorPicture: question.author.picture,
         questionId: question.id,
         comments: question.comment,
         answerCounter: 1,
         likes: question.likes,
         likesCount: question.likes.length,
-        date: dateNotation(question.date)+" gevraagd door"
-      });
-    }
-  });
+        date: question.date
+    });
+  }
 });
+});
+
 
 // POST method route=
 router.post('/', (req, res) => {
@@ -76,10 +77,8 @@ router.post('/', (req, res) => {
     date: Date.now(),
     likes: [],
     author: req.user.id,
-    likes: 0,
-    date: Date.now()
+    comment: []
   });
-  console.log(Date.now());
   question.save(function (err) {
     Question.findOne({}).
     populate('author').
@@ -94,6 +93,7 @@ router.post('/', (req, res) => {
   }))
 });
 
+/*
 function dateNotation(date){
   var milliseconds = (Date.now()-date);
   var seconds = Math.floor(milliseconds/1000);
@@ -133,5 +133,6 @@ function dateNotation(date){
   //return milliseconds+"ms "+seconds+"s "+minutes+"m "+hours+"h "+days+"d "+weeks+"weken "+months+"maanden "+years+"jaar";
   return result;
 }
+*/
 
 module.exports = router;
